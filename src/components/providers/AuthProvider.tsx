8c2738adabdf3c7744d53739e88eec2adb8946b1
sarbeshkc <weebmaniac2314@gmail.com>
2025-03-02 04:11:00 +0545

    Fixed authprovider path

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -3,8 +3,9 @@
 import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '@/lib/firebase/config';
 import { auth } from '@/lib/firebase/config';
+import { db } from '@/lib/firebase/config';
+
 
 interface UserData {
   uid: string;
